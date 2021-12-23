6c263b8660c33ff45d26795f31a1547fa4ed99bb
Vinny Sabatini <vincent.sabatini@gmail.com>
2021-12-23 23:25:35 +0000

    finish day 4 part 2
    This finds the last board to get a bingo. This does not quite work. It
    will print the last board to get a bingo, but it does not print the
    correct last call for that board to get a bingo.

--- 2021/day4/main.go
+++ 2021/day4/main.go
@@ -15,6 +15,10 @@ import (
 // board, and then multiplying that by the last called number. For example, on small-input.txt
 // the third board is the winning board. It sums up to 188, the last number called is 24, and
 // the final total is 4512.
+//
+// Day 4 challenge two takes in the same inputs as challenge 1, but the goal is to figure out
+// the last board that would win. For small-input.txt, the second board would be the last to
+// win, it wins on 13, the remaining tiles sums up to 148, and the final total is 1924.
 func main() {
 	input, err := os.Open("./input.txt")
 	if err != nil {
@@ -32,12 +36,35 @@ func main() {
 		row := strings.Fields(scanner.Text())
 		bingoBoards = append(bingoBoards, row)
 	}
-	for _, call := range bingoCalls {
-		bingoBoards = makeCall(call, bingoBoards)
-		bingo, board := checkBingo(bingoBoards)
+	findWinner(bingoBoards, bingoCalls)
+	findLoser(bingoBoards, bingoCalls)
+}
+
+func findWinner(boards [][]string, calls []string) {
+	for _, call := range calls {
+		boards = makeCall(call, boards)
+		bingo, board := checkBingo(boards)
 		if bingo {
 			fmt.Println("Board", board, "Call", call)
-			printWinningBoard(board, bingoBoards)
+			printBoard(board, boards)
+			break
+		}
+	}
+}
+
+func findLoser(boards [][]string, calls []string) {
+	boardCount := countBoards(boards)
+	var lastBoard int
+	for _, call := range calls {
+		boards = makeCall(call, boards)
+		bingoCount := countBingos(boards)
+		if boardCount == bingoCount {
+			lastBoard = getLastBoard(boards)
+			fmt.Println("Last board", lastBoard)
+		}
+		if boardCount+1 == bingoCount {
+			fmt.Println("Call", call)
+			printBoard(lastBoard, boards)
 			break
 		}
 	}
@@ -52,6 +79,16 @@ func makeCall(call string, boards [][]string) [][]string {
 		}
 	}
 	return boards
+}
+
+func countBoards(boards [][]string) int {
+	count := 0
+	for _, v := range boards {
+		if len(v) == 0 {
+			count = count + 1
+		}
+	}
+	return count
 }
 
 func checkBingo(boards [][]string) (bool, int) {
@@ -80,7 +117,71 @@ func checkBingo(boards [][]string) (bool, int) {
 	return false, 0
 }
 
-func printWinningBoard(board int, boards [][]string) {
+func countBingos(boards [][]string) int {
+	bingoCount := 0
+	bingoCheck := false
+	columnCheck := []int{0, 0, 0, 0, 0}
+	bingoRow := []string{"x", "x", "x", "x", "x"}
+
+	for _, row := range boards {
+		if len(row) == 0 {
+			bingoCheck = false
+			columnCheck = []int{0, 0, 0, 0, 0}
+			continue
+		}
+		if !bingoCheck {
+			if reflect.DeepEqual(row, bingoRow) {
+				bingoCheck = true
+				bingoCount = bingoCount + 1
+				continue
+			}
+			for j, v := range row {
+				if v == "x" {
+					columnCheck[j] = columnCheck[j] + 1
+					if columnCheck[j] == 5 {
+						bingoCheck = true
+						bingoCount = bingoCount + 1
+					}
+				}
+			}
+		}
+	}
+	return bingoCount
+}
+
+func getLastBoard(boards [][]string) int {
+	board := 0
+	columnCheck := []int{0, 0, 0, 0, 0}
+	bingoRow := []string{"x", "x", "x", "x", "x"}
+	isBingo := true
+
+	for _, row := range boards {
+		if len(row) == 0 {
+			if isBingo {
+				board = board + 1
+				columnCheck = []int{0, 0, 0, 0, 0}
+				isBingo = false
+				continue
+			} else {
+				return board
+			}
+		}
+		if reflect.DeepEqual(row, bingoRow) {
+			isBingo = true
+		}
+		for j, v := range row {
+			if v == "x" {
+				columnCheck[j] = columnCheck[j] + 1
+				if columnCheck[j] == 5 {
+					isBingo = true
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func printBoard(board int, boards [][]string) {
 	sum := 0
 	count := 0
 	for _, row := range boards {
