b7bbae81595952606c2d7a44c61b462037070799
Vinny Sabatini <vincent.sabatini@gmail.com>
2021-12-24 17:31:52 +0000

    finish day 6 part 2
    This is the same as part 1 but you have to track for 256 days

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -13,6 +13,10 @@ import (
 // is spawned with a timer of 8, and the parent fish timer resets to 6. The
 // goal is to determine how many fish will exist after 80 days. For example
 // on small-input.txt, the final fish count is 5934.
+//
+// Day 6 challenge 2 does the same thing as part 1, however you have to track
+// how many fish would exist after 256 days. The answer for small-input.txt would
+// be 26984457539.
 func main() {
 	input, err := os.Open("./input.txt")
 	if err != nil {
@@ -22,25 +26,27 @@ func main() {
 	defer input.Close()
 
 	scanner := bufio.NewScanner(input)
-	var liveFish, newFish []int
+	tracker := []int{0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 	scanner.Scan()
 	inputText := strings.Split(scanner.Text(), ",")
 	for _, v := range inputText {
 		vInt, _ := strconv.Atoi(v)
-		liveFish = append(liveFish, vInt)
+		tracker[vInt] = tracker[vInt] + 1
 	}
 
-	for day := 0; day < 80; day++ {
-		newFish = make([]int, 0)
-		for fish, val := range liveFish {
-			liveFish[fish] = val - 1
-			if val == 0 {
-				liveFish[fish] = 6
-				newFish = append(newFish, 8)
-			}
+	for day := 0; day < 256; day++ {
+		newFish := tracker[0]
+		for i := 0; i < len(tracker)-1; i++ {
+			tracker[i] = tracker[i+1]
 		}
-		liveFish = append(liveFish, newFish...)
+		tracker[8] = newFish
+		tracker[6] = tracker[6] + newFish
 	}
-	fmt.Println("Fish Count", len(liveFish))
+	fmt.Println("Tracker", tracker)
+	count := 0
+	for _, v := range tracker {
+		count = count + v
+	}
+	fmt.Println(count)
 }
